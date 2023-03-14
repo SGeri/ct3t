10101d3fe6e5e0e1cabfa92d5d6338c3dcb815dd
SGeri <sarffygeri@gmail.com>
2023-03-14 21:25:04 +0100

    dark version

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => (
-  <ClerkProvider {...pageProps}>
+  <ClerkProvider {...pageProps} appearance={{ baseTheme: dark }}>
     <Component {...pageProps} />
   </ClerkProvider>
 );
