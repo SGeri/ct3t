8421ff07a6603c7d057c1d3e4cd075cdd9c5edcd
SGeri <sarffygeri@gmail.com>
2023-04-15 15:41:53 +0200

    do work on terraform configuration

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,28 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/upstash/upstash" {
+  version     = "1.3.0"
+  constraints = "1.3.0"
+  hashes = [
+    "h1:3dp0IXBdjU76bVZmfSf87LtbWqj9zdRkBoKVPfctvHk=",
+    "zh:12303e624cedf60799757c9f9a6deb057ee8990f36299801e51c123b160a0036",
+    "zh:1f0e1a5f867bea371b572506bc1cdcbcb62df5c1082809ba0e2290154395729b",
+    "zh:21034b62fefee47018c0c60f19f6032c58c1cc304a0a60a76f53891437e720d2",
+    "zh:2421151188585b5cf591566a81f2ffb357683f3b490430a51160fa3bb50de5f4",
+    "zh:68e2a25ec73544d0462d4ac6321561e73202b48e81511650bf1b3bc5848ac0ae",
+    "zh:6ecc992c6489136693943d9c0e799733cc406be1958b0d22f8d200137998de0e",
+    "zh:8fc0ed294ca0dd87898c2d4e0b354b8f49d6a1a4a681bf58a1d885ab1ba510e1",
+    "zh:aad4c374ecb5aca27b745c236116b12667a031e277d9b29f7e9dc0cdeff3e879",
+    "zh:bcc7829733f3d1e1b12a0abaf73105508b767dad46d44679fd56b89077b6ed4c",
+    "zh:d979718d983b858f5fc15bd8dd7490c60741a100fdb2e0ae17472527e9253e42",
+    "zh:ddf4932cdc6432c78db4fc9071bdf8f87d6d809740ef7f33d7bfa39a9a75b0af",
+    "zh:eaee67f9ec41832f35abca3773d8e2b3193d29c645e749f7b9bd7f767a7765c3",
+    "zh:f077d6f279731eb0beff1ef5dc81b5c7e80fdcdc94ff98dbc3bdf42c0126fd86",
+    "zh:f3d21c7b1d9d171a1426c681414acad963c726390f50e7d4d68e71b96ecf3c20",
+  ]
+}
+
 provider "registry.terraform.io/vercel/vercel" {
   version     = "0.11.4"
   constraints = "0.11.4"
--- terraform.tf
+++ terraform.tf
@@ -1,27 +1,91 @@
+/*
+Sadly, Planetscale cannot be used with Terraform, since there is no official provider for it.
+Also, the connection string, that would be supplied to the Vercel environment, is not retrievable from the Planetscale API.
+Therefore, the connection string is supplied manually to the Vercel environment.
+
+[TODO] Current plans are to include an S3 bucket for document uploading and SES Service for emailing in the Terraform config with AWS providers.
+*/
+
 terraform {
   required_providers {
     vercel = {
       source  = "vercel/vercel"
       version = "~> 0.4"
     }
+    upstash = {
+      source  = "upstash/upstash"
+      version = "1.3.0"
+    }
   }
 }
 
+/* --- PROVIDERS --- */
 provider "vercel" {
   api_token = var.VERCEL_API_TOKEN
 }
 
-resource "vercel_project" "example" {
+provider "upstash" {
+  email   = var.UPSTASH_EMAIL
+  api_key = var.UPSTASH_API_KEY
+}
+
+
+/* --- RESOURCES --- */
+resource "vercel_project" "next" {
   name      = "ct3t-next"
   framework = "nextjs"
   git_repository = {
     type = "github"
     repo = "SGeri/ct3t"
   }
+
   root_directory = "apps/next"
   build_command  = "cd ../.. && npm run build"
+
+  environment = [
+    {
+      key    = "DATABASE_URL"
+      value  = var.PLANETSCALE_DB_URL
+      target = ["development", "preview", "production"]
+    },
+    {
+      key    = "UPSTASH_REDIS_REST_URL"
+      value  = data.upstash_redis_database_data.redis_data.endpoint
+      target = ["development", "preview", "production"]
+    },
+    {
+      key    = "UPSTASH_REDIS_REST_TOKEN"
+      value  = data.upstash_redis_database_data.redis_data.rest_token
+      target = ["development", "preview", "production"]
+    },
+    {
+      key    = "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY"
+      value  = var.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+      target = ["development", "preview", "production"]
+    },
+    {
+      key    = "CLERK_SECRET_KEY"
+      value  = var.CLERK_SECRET_KEY
+      target = ["development", "preview", "production"]
+    },
+  ]
 }
 
-variable "VERCEL_API_TOKEN" {
-  type = string
+resource "upstash_redis_database" "redis" {
+  database_name = "ct3t-redis"
+  region        = "eu-central-1"
+  tls           = "true"
 }
+
+/* --- DATA SOURCES --- */
+data "upstash_redis_database_data" "redis_data" {
+  database_id = resource.upstash_redis_database.redis.database_id
+}
+
+/* --- VARIABLES (from env) --- */
+variable "VERCEL_API_TOKEN" {}
+variable "UPSTASH_EMAIL" {}
+variable "UPSTASH_API_KEY" {}
+variable "PLANETSCALE_DB_URL" {}
+variable "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY" {}
+variable "CLERK_SECRET_KEY" {}
