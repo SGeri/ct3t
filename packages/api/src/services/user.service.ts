09eff9db0d3001743f90e74bb608796603faeb05
SGeri <sarffygeri@gmail.com>
2023-04-17 22:51:24 +0200

    add cache key to the user service

--- packages/api/src/services/user.service.ts
+++ packages/api/src/services/user.service.ts
@@ -2,10 +2,11 @@ import { prisma, redis, type User } from "@packages/db";
 import type { getUserInput } from "./user.types";
 
 const isProd = process.env.NODE_ENV === "production";
+const CACHE_KEY_PREFIX = "user:";
 
 class UserService {
-  redis = redis;
-  prisma = prisma;
+  readonly redis = redis;
+  readonly prisma = prisma;
 
   // Always fetches the user from the database
   async getUser(input: getUserInput) {
@@ -16,9 +17,11 @@ class UserService {
 
   // Cached user will be reutrned, otherwise it will be fetched from the database
   async getUserByClerkId(clerkId: string) {
+    const cacheKey = CACHE_KEY_PREFIX + clerkId;
+
     // disable user caching in dev
     const cachedUser: User | null = isProd
-      ? await this.redis.get(clerkId)
+      ? await this.redis.get(cacheKey)
       : null;
 
     if (cachedUser) return cachedUser;
@@ -27,13 +30,15 @@ class UserService {
 
     if (!user) return null;
 
-    await this.redis.set(clerkId, user);
+    await this.redis.set(cacheKey, user);
     return user;
   }
 
   // Invalidates the user cache
   async invalidateUserCache(clerkId: string) {
-    await this.redis.del(clerkId);
+    const cacheKey = CACHE_KEY_PREFIX + clerkId;
+
+    await this.redis.del(cacheKey);
   }
 }
 
